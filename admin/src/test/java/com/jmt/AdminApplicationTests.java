e2d62ffcf6374be6a32976b52c6f9d53a13cc339
yonggyo1125 <yonggyo00@kakao.com>
2024-08-01 20:52:16 +0900

    관리자 웹 설정

---
+++ admin/src/test/java/com/jmt/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
