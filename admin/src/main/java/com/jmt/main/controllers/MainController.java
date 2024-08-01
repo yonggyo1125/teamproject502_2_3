68885f05d9f704f155782a2c699c6b6bb0829421
yonggyo1125 <yonggyo00@kakao.com>
2024-08-01 21:13:46 +0900

    .

---
+++ admin/src/main/java/com/jmt/main/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.jmt.main.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String index() {
+        return "관리자 메인";
+    }
+}
