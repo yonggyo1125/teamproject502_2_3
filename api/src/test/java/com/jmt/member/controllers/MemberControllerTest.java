fb5f85eeb7ef7d9d787f45ef4058c1aa4d676e77
yonggyo1125 <yonggyo00@kakao.com>
2024-08-02 15:35:07 +0900

    소스

--- api/src/test/java/com/jmt/member/controllers/MemberControllerTest.java
+++ api/src/test/java/com/jmt/member/controllers/MemberControllerTest.java
@@ -1,10 +1,19 @@
 package com.jmt.member.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -13,4 +22,27 @@ public class MemberControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper om;
+    
+    @Test
+    @DisplayName("회원 가입 테스트")
+    void joinTest() throws Exception {
+        RequestJoin form = new RequestJoin();
+        form.setEmail("user01@test.org");
+        form.setPassword("_aA123456");
+        form.setConfirmPassword(form.getPassword());
+        form.setUserName("사용자01");
+        form.setMobile("010-1000-1000");
+        form.setAgree(true);
+
+        String params = om.writeValueAsString(form);
+
+        mockMvc.perform(post("/account")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(Charset.forName("UTF-8"))
+                        .content(params))
+                .andDo(print());
+    }
 }
