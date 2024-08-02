dc9f6fae94a6118b3dff4469b77fe83a3891a75a
yonggyo1125 <yonggyo00@kakao.com>
2024-08-02 15:01:32 +0900

    소스

--- api/src/main/java/com/jmt/member/controllers/MemberController.java
+++ api/src/main/java/com/jmt/member/controllers/MemberController.java
@@ -1,12 +1,32 @@
 package com.jmt.member.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jmt.member.validators.JoinValidator;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/account")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final JoinValidator joinValidator;
+
+    @PostMapping
+    public ResponseEntity join(@RequestBody @Valid RequestJoin form, Errors errors) {
+
+        joinValidator.validate(form, errors);
+
+        if (errors.hasErrors()) {
+
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 
     @GetMapping("/token")
     public String token() {
