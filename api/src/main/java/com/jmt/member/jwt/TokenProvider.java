eb917daa87dea28279545148391c045095ad509f
yonggyo1125 <yonggyo00@kakao.com>
2024-08-03 14:28:12 +0900

    소스

--- api/src/main/java/com/jmt/member/jwt/TokenProvider.java
+++ api/src/main/java/com/jmt/member/jwt/TokenProvider.java
@@ -2,6 +2,8 @@ package com.jmt.member.jwt;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +12,19 @@ import org.springframework.stereotype.Component;
 @EnableConfigurationProperties(JwtProperties.class)
 public class TokenProvider {
 
-    public String createToken() {
 
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    /**
+     * JWT 토큰 생성
+     *
+     * @param email
+     * @param password
+     * @return
+     */
+    public String createToken(String email, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+        Authentication authentication =
         return null;
     }
 
