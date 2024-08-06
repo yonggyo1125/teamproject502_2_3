e3247f5e25e96c7f89e1445d1a9326c5d0535e24
yonggyo1125 <yonggyo00@kakao.com>
2024-08-06 09:00:20 +0900

    관리자 기능 수정 중

--- admin/src/main/java/com/jmt/main/controllers/MainController.java
+++ admin/src/main/java/com/jmt/main/controllers/MainController.java
@@ -1,15 +1,39 @@
 package com.jmt.main.controllers;
 
+import com.jmt.global.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.util.List;
+
+@Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class MainController {
-
+    private final Utils utils;
     @GetMapping
-    public String index() {
-        return "관리자 메인";
+    public String index(Model model, HttpServletRequest request) {
+        System.out.println(request.getHeader("from-gateway"));
+        model.addAttribute("addCss", List.of("member/test1", "member/test2"));
+        model.addAttribute("addScript", List.of("member/test1", "member/test2"));
+
+        return "main/index";
+    }
+    /*
+    @GetMapping("/main2")
+    public String index2() {
+
+        //return utils.redirectUrl("/test2");
+    }
+    */
+    @ResponseBody
+    @GetMapping("/test2")
+    public void test2() {
+
     }
 }
