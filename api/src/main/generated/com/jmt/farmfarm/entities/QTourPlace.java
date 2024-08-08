d4cad98feeaacbb3af5008fb1b7196c79ec89820
yonggyo1125 <yonggyo00@kakao.com>
2024-08-08 14:54:56 +0900

    3조 공공 데이터 처리

---
+++ api/src/main/generated/com/jmt/farmfarm/entities/QTourPlace.java
@@ -0,0 +1,64 @@
+package com.jmt.farmfarm.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QTourPlace is a Querydsl query type for TourPlace
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QTourPlace extends EntityPathBase<TourPlace> {
+
+    private static final long serialVersionUID = -1694252550L;
+
+    public static final QTourPlace tourPlace = new QTourPlace("tourPlace");
+
+    public final com.jmt.global.entities.QBaseEntity _super = new com.jmt.global.entities.QBaseEntity(this);
+
+    public final StringPath address = createString("address");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = _super.deletedAt;
+
+    public final StringPath description = createString("description");
+
+    public final NumberPath<Double> latitude = createNumber("latitude", Double.class);
+
+    public final NumberPath<Double> longitude = createNumber("longitude", Double.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final StringPath photoUrl = createString("photoUrl");
+
+    public final NumberPath<Long> seq = createNumber("seq", Long.class);
+
+    public final StringPath tel = createString("tel");
+
+    public final StringPath title = createString("title");
+
+    public final NumberPath<Integer> tourDays = createNumber("tourDays", Integer.class);
+
+    public QTourPlace(String variable) {
+        super(TourPlace.class, forVariable(variable));
+    }
+
+    public QTourPlace(Path<? extends TourPlace> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QTourPlace(PathMetadata metadata) {
+        super(TourPlace.class, metadata);
+    }
+
+}
+
