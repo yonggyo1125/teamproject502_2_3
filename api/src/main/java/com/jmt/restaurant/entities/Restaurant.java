8909d123096f54b0ff71f681e4dcf703f7076d95
yonggyo1125 <yonggyo00@kakao.com>
2024-08-08 17:43:24 +0900

    2조 공공 데이터 처리

---
+++ api/src/main/java/com/jmt/restaurant/entities/Restaurant.java
@@ -0,0 +1,54 @@
+package com.jmt.restaurant.entities;
+
+import com.jmt.global.entities.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Restaurant extends BaseEntity {
+
+    @Id
+    private Long rstrId;
+    private String rstrNm; // 식당명
+    private String rstrRdnmAdr; // 도로명 주소
+    private String rstrLnnoAdres; // 구주소
+    private Double rstrLa; // 위도
+    private Double rstrLo; // 경도
+    private Double rstrTelNo; // 전화번호
+    private Double dbsnsStatmBzcndNm;  // 영업신고증업태명
+    private Double bsnsLcncNm; // 영업인허가명
+    @Lob
+    private String rstrIntrcnCont; // 음식점소개내용
+
+    private String areaNm; // 지역명
+    private Integer prdlSeatCnt; // 입식좌석수
+    private Integer seatCnt; // 좌식좌석수
+    private Boolean prkgPosYn; // 주차가능여부
+    private Boolean wifiOfrYn; // Wifi 제공여부
+    private Boolean dcrnYn; // 놀이방유무
+    private Boolean petEntrnPosblYn; // 반려동물입장가능여부
+    private Boolean fgggMenuOfrYn; // 다국어메뉴판제공여부
+    private String tlromInfoCn; // 화장실정보내용
+    private String restdyInfoCn; // 휴무일정보내용
+    private String bsnsTmCn; // 영업시간내용
+    private Boolean hmdlvSaleYn; // 택배판매유무
+    private Boolean dsbrCvntlYn; // 배리어프리유무
+    private Boolean delvSrvicYn; // 배달서비스유무
+    private String rsrvMthdNm; // 예약방식명
+    private String onlineRsrvInfoCn; // 온라인예약정보내용
+    private String hmpgUrl; // 홈페이지 URL
+    private Boolean kioskYn; // 키오스크 유무
+    private Boolean mbPmamtYn; // 모바일페이먼트유무
+    private Boolean smorderYn; // 스마트오더유무
+    private String reprsntMenuNm; // 대표메뉴명
+    private String RSTR_IMG_URL;
+
+}
