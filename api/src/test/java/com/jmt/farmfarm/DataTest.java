3e5179970fb3ee7261e0875c4cb3ed4e971d9787
yonggyo1125 <yonggyo00@kakao.com>
2024-08-08 16:00:53 +0900

    3조 공공 데이터 처리

--- api/src/test/java/com/jmt/farmfarm/DataTest.java
+++ api/src/test/java/com/jmt/farmfarm/DataTest.java
@@ -3,13 +3,17 @@ package com.jmt.farmfarm;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jmt.farmfarm.entities.TourPlace;
+import com.jmt.farmfarm.entities.TourPlaceTag;
 import com.jmt.farmfarm.repositories.TourPlaceRepository;
+import com.jmt.farmfarm.repositories.TourPlaceTagRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +26,9 @@ public class DataTest {
 
     @Autowired
     private TourPlaceRepository repository;
+
+    @Autowired
+    private TourPlaceTagRepository tagRepository;
 
     @Test
     void test1() throws Exception {
@@ -48,19 +55,30 @@ public class DataTest {
         File file = new File("D:/data/data2.json");
         List<Map<String, String>> tmp = om.readValue(file, new TypeReference<>() {});
 
-        List<TourPlace> items = tmp.stream()
-                .map(d -> TourPlace.builder()
-                        .title(d.get("주제"))
-                        .sido(d.get("시도"))
-                        .sigungu(d.get("시군구"))
-                        .address(d.get("시도") + " " + d.get("시군구"))
-                        .description(d.get("요약"))
-                        .period(d.get("월"))
-                        .photoUrl(d.get("사진파일"))
-                        .course(d.get("코스정보")).build()
-                ).toList();
+       tmp.forEach(d -> {
+                    String tagsTmp = d.get("태그");
+                    List<TourPlaceTag> tags = null;
+                    if (StringUtils.hasText(tagsTmp)) {
+                       tags = Arrays.stream(tagsTmp.split(","))
+                                .map(s -> TourPlaceTag.builder().tag(s).build())
+                                .toList();
+                        tagRepository.saveAllAndFlush(tags);
+                    }
 
-        repository.saveAllAndFlush(items);
+                    TourPlace item = TourPlace.builder()
+                            .tags(tags)
+                           .title(d.get("주제"))
+                           .sido(d.get("시도"))
+                           .sigungu(d.get("시군구"))
+                           .address(d.get("시도") + " " + d.get("시군구"))
+                           .description(d.get("요약"))
+                           .period(d.get("월"))
+                           .photoUrl(d.get("사진파일"))
+                           .course(d.get("코스정보")).build();
+                    repository.saveAndFlush(item);
+                 });
+
+
 
     }
 }
