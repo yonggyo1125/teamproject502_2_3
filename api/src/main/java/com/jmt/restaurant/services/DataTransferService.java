3a1a3496ad1f91035c2e860546cedfe238a6338b
yonggyo1125 <yonggyo00@kakao.com>
2024-08-09 19:00:17 +0900

    2조 공공 데이터 처리

--- api/src/main/java/com/jmt/restaurant/services/DataTransferService.java
+++ api/src/main/java/com/jmt/restaurant/services/DataTransferService.java
@@ -6,7 +6,11 @@ import com.jmt.restaurant.repositories.RestaurantImageRepository;
 import com.jmt.restaurant.repositories.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @Lazy
 @Service
@@ -17,11 +21,17 @@ public class DataTransferService {
     private final FoodMenuRepository foodMenuRepository;
     private final FoodMenuImageRepository foodMenuImageRepository;
 
+    private final RestTemplate restTemplate;
+    private String serviceKey = "BOYbPA7XwfKbTICZBI6Myilx082zrTanL2VbWfQxwHMa8WA1YwyN2PkhdIeJZkmL";
+
     /**
      * 식당 기본 정보
      *
      */
     public void update1() {
+        String url = String.format("https://seoul.openapi.redtable.global/api/rstr?serviceKey=%s", serviceKey);
 
+        ResponseEntity<String> response = restTemplate.getForEntity(URI.create(url), String.class);
+        System.out.println(response);
     }
 }
--- api/src/test/java/com/jmt/restaurant/DataTransfer.java
+++ api/src/test/java/com/jmt/restaurant/DataTransfer.java
@@ -1,7 +1,9 @@
 package com.jmt.restaurant;
 
-import com.jmt.restaurant.repositories.RestaurantImageRepository;
-import com.jmt.restaurant.repositories.RestaurantRepository;
+import com.jmt.restaurant.services.DataTransferService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -9,6 +11,12 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 public class DataTransfer {
 
+    @Autowired
+    private DataTransferService service;
 
-
+    @Test
+    @DisplayName("식당 기본 정보")
+    void update1() {
+        service.update1();
+    }
 }
