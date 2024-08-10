d0438b5e39673b6901b6a8e2222ca6a7e746a35a
yonggyo1125 <yonggyo00@kakao.com>
2024-08-10 10:48:02 +0900

    2조 공공 데이터 처리

--- api/src/main/java/com/jmt/restaurant/services/DataTransferService.java
+++ api/src/main/java/com/jmt/restaurant/services/DataTransferService.java
@@ -61,7 +61,7 @@ public class DataTransferService {
 
         List<Restaurant> items = tmp.stream()
                 .map(d -> {
-                        Map<String, String> extra = getExtra(tmp2, d.get("RSTR_ID"));
+                        Map<String, String> extra = getExtra(tmp2, d.get("RSTR_ID"), "RSTR");
 
                         Restaurant rest =  Restaurant.builder()
                             .rstrId(Long.valueOf(d.get("RSTR_ID")))
@@ -193,7 +193,7 @@ public class DataTransferService {
                             .spcltMenuNm(d.get("SPCLT_MENU_NM"))
                             .build();
 
-                    Map<String, String> extra = getExtra(tmp2, d.get("RSTR_ID"));
+                    Map<String, String> extra = getExtra(tmp2, d.get("MENU_ID"), "MENU");
                     if (extra != null) {
                         food.setMenuDscrn(extra.get("MENU_DSCRN"));
                         food.setMenuCtgryLclasNm(extra.get("MENU_CTGRY_LCLAS_NM"));
@@ -244,11 +244,11 @@ public class DataTransferService {
             foodMenuImageRepository.saveAllAndFlush(items);
     }
 
-    private Map<String, String> getExtra(List<Map<String, String>> items, String rstrId) {
+    private Map<String, String> getExtra(List<Map<String, String>> items, String id, String mode) {
         if (items == null || items.isEmpty()) return null;
 
         return items.stream()
-                .filter(d -> d.get("RSTR_ID").equals(rstrId))
+                .filter(d -> d.get(mode + "_ID").equals(id))
                 .findFirst().orElse(null);
     }
 }
