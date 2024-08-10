4e7173da6e0309a2af338cc08983fa623058c14a
yonggyo1125 <yonggyo00@kakao.com>
2024-08-10 12:40:09 +0900

    .

--- ai/src/main/java/com/jmt/ai/controllers/ChatController.java
+++ ai/src/main/java/com/jmt/ai/controllers/ChatController.java
@@ -1,7 +1,9 @@
 package com.jmt.ai.controllers;
 
+import com.jmt.global.rests.JSONData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +15,13 @@ public class ChatController {
     //private final VertexAiGeminiChatModel geminiChatModel;
 
     @RequestMapping("/")
-    public void chat(@RequestParam("message") String message) {
+    public JSONData chat(@RequestParam(name="message", required = false) String message) {
+        if (!StringUtils.hasText(message)) {
+            throw new IllegalArgumentException("메세지를 입력하세요.");
+        }
+
        String openAiResponse = openAiChatModel.call(message);
 
-        /*
-        String geminiResponse = geminiChatModel.call(message);
-        //System.out.println(openAiResponse);
-        System.out.println(geminiResponse);
-        */
+        return new JSONData(openAiResponse);
     }
 }
