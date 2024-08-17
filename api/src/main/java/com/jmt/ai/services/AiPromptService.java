e30d66bda4c9127e483e45b6e1540801a480f9a9
yonggyo1125 <yonggyo00@kakao.com>
2024-08-17 12:37:20 +0900

    3조 파일

--- api/src/main/java/com/jmt/ai/services/AiPromptService.java
+++ api/src/main/java/com/jmt/ai/services/AiPromptService.java
@@ -1,11 +1,18 @@
 package com.jmt.ai.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jmt.ai.controllers.RequestMessage;
 import com.jmt.global.services.ConfigInfoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +21,8 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AiPromptService {
     private final ConfigInfoService infoService;
+    private final ObjectMapper om;
+    private final RestTemplate restTemplate;
 
     public String prompt(String message) {
         Map<String, String> config = infoService.getApiConfig();
@@ -29,7 +38,23 @@ public class AiPromptService {
         data.put("role", "user");
         data.put("content", message);
         RequestMessage params = new RequestMessage(List.of(data));
+        try {
+            String json = om.writeValueAsString(params);
 
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(token);
+            headers.setContentType(MediaType.APPLICATION_JSON);
 
+            HttpEntity<String> request = new HttpEntity<>(json, headers);
+
+            ResponseEntity<String> response = restTemplate.postForEntity(URI.create(url), request, String.class);
+
+            System.out.println(response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
