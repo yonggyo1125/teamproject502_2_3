2bc60b2e54489c354b8c43f58e0e159da5fdf142
yonggyo1125 <yonggyo00@kakao.com>
2024-08-17 11:11:30 +0900

    관리자 게시판 API

---
+++ admin/src/main/java/com/jmt/api/config/ApiConfigController.java
@@ -0,0 +1,81 @@
+package com.jmt.api.config;
+
+import com.jmt.config.controllers.ApiConfig;
+import com.jmt.config.controllers.BasicConfig;
+import com.jmt.config.service.ConfigInfoService;
+import com.jmt.global.exceptions.RestExceptionProcessor;
+import com.jmt.global.exceptions.UnAuthorizedException;
+import com.jmt.global.rests.JSONData;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/config")
+@RequiredArgsConstructor
+public class ApiConfigController implements RestExceptionProcessor {
+
+    @Value("${secretKey}")
+    private String secretKey;
+
+    private final PasswordEncoder encoder;
+    private final ConfigInfoService infoService;
+    private final HttpServletRequest request;
+
+    @GetMapping
+    public ResponseEntity<JSONData> siteConfig() {
+        checkToken();
+
+        BasicConfig config = infoService.get("basic", BasicConfig.class).orElse(null);
+
+        JSONData data = new JSONData();
+        data.setSuccess(config != null);
+        if (config == null) {
+            data.setStatus(HttpStatus.NOT_FOUND);
+        }
+        data.setData(data);
+
+        return ResponseEntity.status(data.getStatus()).body(data);
+    }
+
+    @GetMapping("/apikeys")
+    public ResponseEntity<JSONData> apiKeys() {
+
+        checkToken();
+
+        ApiConfig config = infoService.get("apiConfig", ApiConfig.class).orElse(null);
+
+        JSONData data = new JSONData();
+        data.setSuccess(config != null);
+        if (config == null) {
+            data.setStatus(HttpStatus.NOT_FOUND);
+        }
+        data.setData(config);
+
+        return ResponseEntity.status(data.getStatus()).body(data);
+    }
+
+    private void checkToken() {
+        /**
+         * 요청 헤더
+         * Authorization: Bearer BCrypt 해시
+         */
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken == null || !StringUtils.hasText(bearerToken.trim())) {
+            throw new UnAuthorizedException();
+        }
+
+        String token = bearerToken.substring(7);
+        if (!encoder.matches(secretKey, token)) {
+            throw new UnAuthorizedException();
+        }
+    }
+
+}
--- admin/src/main/java/com/jmt/config/controllers/ApiConfig.java
+++ admin/src/main/java/com/jmt/config/controllers/ApiConfig.java
@@ -7,4 +7,6 @@ public class ApiConfig {
     private String publicOpenApiKey; // 공공 API 인증키
 
     private String kakaoJavascriptKey; // 카카오 API - 자바스크립트 앱 키
+
+    private String huggingfaceAccessToken;
 }
