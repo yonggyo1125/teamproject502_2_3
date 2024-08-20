476c40ea42fba4b3e30a5bc1ed0c51e6ecbbafe0
yonggyo1125 <yonggyo00@kakao.com>
2024-08-20 14:47:53 +0900

    3조 파일

--- api/src/main/generated/com/jmt/activity/entities/QReservation.java
+++ api/src/main/generated/com/jmt/activity/entities/QReservation.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,9 +18,17 @@ public class QReservation extends EntityPathBase<Reservation> {
 
     private static final long serialVersionUID = 1630537652L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QReservation reservation = new QReservation("reservation");
 
     public final com.jmt.global.entities.QBaseEntity _super = new com.jmt.global.entities.QBaseEntity(this);
+
+    public final QActivity activity;
+
+    public final StringPath activityName = createString("activityName");
+
+    public final EnumPath<com.jmt.activity.constants.AM_PM> ampm = createEnum("ampm", com.jmt.activity.constants.AM_PM.class);
 
     //inherited
     public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
@@ -27,19 +36,51 @@ public class QReservation extends EntityPathBase<Reservation> {
     //inherited
     public final DateTimePath<java.time.LocalDateTime> deletedAt = _super.deletedAt;
 
+    public final StringPath doroAddress = createString("doroAddress");
+
+    public final StringPath email = createString("email");
+
+    public final com.jmt.member.entities.QMember member;
+
+    public final StringPath mobile = createString("mobile");
+
     //inherited
     public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
 
+    public final StringPath name = createString("name");
+
+    public final StringPath ownerName = createString("ownerName");
+
+    public final StringPath ownerTel = createString("ownerTel");
+
+    public final NumberPath<Integer> persons = createNumber("persons", Integer.class);
+
+    public final DatePath<java.time.LocalDate> rDate = createDate("rDate", java.time.LocalDate.class);
+
+    public final NumberPath<Long> seq = createNumber("seq", Long.class);
+
+    public final StringPath townName = createString("townName");
+
     public QReservation(String variable) {
-        super(Reservation.class, forVariable(variable));
+        this(Reservation.class, forVariable(variable), INITS);
     }
 
     public QReservation(Path<? extends Reservation> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QReservation(PathMetadata metadata) {
-        super(Reservation.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReservation(PathMetadata metadata, PathInits inits) {
+        this(Reservation.class, metadata, inits);
+    }
+
+    public QReservation(Class<? extends Reservation> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.activity = inits.isInitialized("activity") ? new QActivity(forProperty("activity")) : null;
+        this.member = inits.isInitialized("member") ? new com.jmt.member.entities.QMember(forProperty("member")) : null;
     }
 
 }
--- api/src/main/java/com/jmt/activity/entities/Reservation.java
+++ api/src/main/java/com/jmt/activity/entities/Reservation.java
@@ -1,15 +1,49 @@
 package com.jmt.activity.entities;
 
+import com.jmt.activity.constants.AM_PM;
 import com.jmt.global.entities.BaseEntity;
-import jakarta.persistence.Entity;
+import com.jmt.member.entities.Member;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @Entity
 @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class Reservation extends BaseEntity {
+    @Id @GeneratedValue
+    private Long seq;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    private Activity activity;
+
+    private String name; // 예약자명
+
+    private String email; // 예약자 이메일
+
+    private String mobile; // 예약자 휴대전화번호
+
+    private String townName; // 체험마을명
+    
+    private String activityName; // 체험프로그램명
+
+    private String doroAddress; // 주소
+
+    private String ownerName; // 대표자명
+
+    private String ownerTel; // 대표자 전화번호
+
+    private LocalDate rDate; // 예약일
+
+    private AM_PM ampm; // 오전/오후 구분
+
+    private int persons; // 예약인원수
 }
