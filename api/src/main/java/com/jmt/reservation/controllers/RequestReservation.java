31b891a5d9bb856579aa6e8ce17129cdcfaeee7e
yonggyo1125 <yonggyo00@kakao.com>
2024-08-21 16:56:55 +0900

    3조 체험활동 처리

--- api/src/main/java/com/jmt/reservation/controllers/RequestReservation.java
+++ api/src/main/java/com/jmt/reservation/controllers/RequestReservation.java
@@ -1,6 +1,9 @@
 package com.jmt.reservation.controllers;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,9 +11,17 @@ import java.time.LocalTime;
 
 @Data
 public class RequestReservation {
-    private Long rstrId;
+
+    @NotNull
+    private Long rstrId; // 식당 등록 번호
+
+    @NotBlank
     private String name; // 예약자명
+
+    @NotBlank
     private String email; // 예약자 이메일
+
+    @NotBlank
     private String mobile; // 예약자 휴대전화번호
 
     @JsonFormat(pattern="yyyy-MM-dd")
@@ -21,5 +32,6 @@ public class RequestReservation {
 
     private String payMethod; // 결제 수단
 
+    @Min(1)
     private int persons; // 예약 인원수
 }
--- api/src/main/java/com/jmt/reservation/validators/ReservationValidator.java
+++ api/src/main/java/com/jmt/reservation/validators/ReservationValidator.java
@@ -1,12 +1,24 @@
 package com.jmt.reservation.validators;
 
 import com.jmt.reservation.controllers.RequestReservation;
+import com.jmt.restaurant.entities.Restaurant;
+import com.jmt.restaurant.services.RestaurantInfoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+
 @Component("reservationValidator2")
+@RequiredArgsConstructor
 public class ReservationValidator implements Validator {
+
+    private final RestaurantInfoService infoService;
+
     @Override
     public boolean supports(Class<?> clazz) {
         return clazz.isAssignableFrom(RequestReservation.class);
@@ -14,6 +26,44 @@ public class ReservationValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+        RequestReservation form = (RequestReservation) target;
+
+        Long rstrId = form.getRstrId(); // 식당 등록 번호
+        Restaurant restaurant = infoService.get(rstrId);
+
+        LocalDate rDate = form.getRDate(); // 예약일
+        LocalTime rTime = form.getRTime(); // 예약시간
+
+
+        // 예약일 검증 S
+        List<LocalDate> availableDates = restaurant.getAvailableDates();
+        if (availableDates == null || availableDates.isEmpty() || !availableDates.contains(rDate)) {
+            errors.rejectValue("rDate", "NotAvailable.restaurant");
+        } else { // 예약 가능일인 경우 예약 시간도 검증
+            Map<String, List<LocalTime>> availableTimes = restaurant.getAvailableTimes();
+            int _yoil = rDate.getDayOfWeek().getValue(); // 1(월) ~ 7(일)
+            boolean possible = true;
+            for (Map.Entry<String, List<LocalTime>> entry : availableTimes.entrySet()) {
+                String yoil = entry.getKey();
+                if (yoil.equals("평일") && _yoil > 5) {
+                    possible = false;
+                } else if (yoil.equals("토요일") && _yoil != 6) {
+                    possible = false;
+                } else if (yoil.equals("일요일") && _yoil != 7) {
+                    possible = false;
+                } else if (yoil.equals("주말") && ! List.of(6,7).contains(_yoil)) {
+                    possible = false;
+                }
+                if (!possible) break;
+            }
+
+            if (!possible) {
+                errors.rejectValue("rTime", "NotAvailable.restaurant");
+            }
+        }
+        // 예약일 검증 E
+
+
 
     }
 }
