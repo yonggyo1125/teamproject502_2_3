f74578bdf4cefb95936238ed5a02d8f15cd67a27
yonggyo1125 <yonggyo00@kakao.com>
2024-08-21 17:00:39 +0900

    3조 체험활동 처리

--- api/src/main/java/com/jmt/reservation/controllers/ReservationController.java
+++ api/src/main/java/com/jmt/reservation/controllers/ReservationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("reservationController2")
 @RequestMapping("/reservation")
 @RequiredArgsConstructor
 public class ReservationController {
