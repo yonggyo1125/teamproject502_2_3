2bab27e2523bbccd9efd97892cb53bb32cac36c4
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 10:29:26 +0900

    2,3조 찜하기 처리

---
+++ api/src/main/java/com/jmt/board/entities/BoardView.java
@@ -0,0 +1,23 @@
+package com.jmt.board.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(BoardViewId.class)
+public class BoardView {
+    @Id
+    private Long seq; // 게시글 번호
+
+    @Id
+    @Column(name="_uid")
+    private Integer uid; // 비회원 - guestUid(), 회원 - 회원번호
+}
---
+++ api/src/main/java/com/jmt/board/entities/BoardViewId.java
@@ -0,0 +1,13 @@
+package com.jmt.board.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardViewId {
+    private Long seq;
+    private Integer uid;
+}
