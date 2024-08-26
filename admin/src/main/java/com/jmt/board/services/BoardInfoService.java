e598b0cfa341eb96268344a5f5ce10d3a5b55278
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 14:50:01 +0900

    2,3조 찜하기 처리

---
+++ admin/src/main/java/com/jmt/board/services/BoardInfoService.java
@@ -0,0 +1,9 @@
+package com.jmt.board.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardInfoService {
+}
--- api/src/main/java/com/jmt/global/configs/BeanConfig.java
+++ api/src/main/java/com/jmt/global/configs/BeanConfig.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -15,11 +18,13 @@ public class BeanConfig {
 
     private final EntityManager em;
 
+    @Lazy
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(em);
     }
 
+    @Lazy
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper om = new ObjectMapper();
@@ -28,8 +33,18 @@ public class BeanConfig {
         return om;
     }
 
+    @Lazy
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Lazy
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
     }
 }
