c8ac8188fe4cfc0efdca020745dff953cc0fe8fb
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 15:27:19 +0900

    관리자 게시판 API

--- admin/src/main/java/com/jmt/board/services/BoardInfoService.java
+++ admin/src/main/java/com/jmt/board/services/BoardInfoService.java
@@ -7,8 +7,12 @@ import com.jmt.board.entities.BoardData;
 import com.jmt.global.ListData;
 import com.jmt.global.Utils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +20,15 @@ public class BoardInfoService {
     private final RestTemplate restTemplate;
     private final ObjectMapper om;
     private final Utils utils;
+
+    public HttpHeaders getHeaders(String method) {
+        HttpHeaders headers = new HttpHeaders();
+        if (!List.of("GET", "DELETE").contains(method)) { // GET, DELETE 이외 방식은 모두 Body 데이터 있다.
+            headers.setContentType(MediaType.APPLICATION_JSON);
+        }
+
+        return headers;
+    }
 
     public ListData<BoardData> getList(BoardDataSearch search) {
         String url = utils.url("/board/admin", "api-service");
