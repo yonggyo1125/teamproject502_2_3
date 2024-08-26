2ec182d3756525d0f383d1b0eec94f5274e2e8e1
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 14:40:55 +0900

    2,3조 찜하기 처리

--- api/src/main/java/com/jmt/board/controllers/BoardAdminController.java
+++ api/src/main/java/com/jmt/board/controllers/BoardAdminController.java
@@ -2,11 +2,14 @@ package com.jmt.board.controllers;
 
 import com.jmt.board.entities.BoardData;
 import com.jmt.board.services.BoardInfoService;
+import com.jmt.board.services.admin.BoardAdminService;
 import com.jmt.global.ListData;
 import com.jmt.global.constants.DeleteStatus;
 import com.jmt.global.rests.JSONData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/board/admin")
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class BoardAdminController {
 
     private final BoardInfoService boardInfoService;
+    private final BoardAdminService boardAdminService;
 
     @GetMapping // 목록 조회
     public JSONData getList(BoardDataSearch search) {
@@ -24,12 +28,17 @@ public class BoardAdminController {
 
     @PatchMapping("/{mode}") // 목록 수정, 삭제
     public JSONData updatelist(@PathVariable("mode") String mode, @RequestBody RequestAdminList form) {
+        List<BoardData> items = boardAdminService.update(mode, form.getItems());
 
-
-        return null;
+        return new JSONData(items);
     }
 
-    
+    @PatchMapping("/{mode}/{seq}") // 게시글 하나 수정, 삭제
+    public JSONData update(@PathVariable("mode") String mode, @PathVariable("seq") Long seq, RequestBoard form) {
+        form.setSeq(seq);
+
+        BoardData item = boardAdminService.update(mode, form);
+    }
 
     @GetMapping("/info/{seq}")
     public JSONData getInfo(Long seq) {
