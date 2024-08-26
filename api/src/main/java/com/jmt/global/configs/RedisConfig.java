cc3840d3a9a144f7614fb0a298564caeeda95a57
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 10:47:40 +0900

    2,3조 찜하기 처리

--- api/src/main/java/com/jmt/global/configs/RedisConfig.java
+++ api/src/main/java/com/jmt/global/configs/RedisConfig.java
@@ -3,8 +3,14 @@ package com.jmt.global.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
