c5526cd012da5de6a6b7615148a6af0e116e6f30
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 15:30:21 +0900

    관리자 게시판 API

--- admin/src/main/java/com/jmt/board/services/BoardInfoService.java
+++ admin/src/main/java/com/jmt/board/services/BoardInfoService.java
@@ -6,6 +6,10 @@ import com.jmt.board.controllers.BoardDataSearch;
 import com.jmt.board.entities.BoardData;
 import com.jmt.global.ListData;
 import com.jmt.global.Utils;
+import com.jmt.global.exceptions.UnAuthorizedException;
+import com.jmt.member.entities.JwtToken;
+import com.jmt.member.repositories.JwtTokenRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -20,9 +24,16 @@ public class BoardInfoService {
     private final RestTemplate restTemplate;
     private final ObjectMapper om;
     private final Utils utils;
+    private final JwtTokenRepository jwtTokenRepository;
+    private final HttpSession session;
 
     public HttpHeaders getHeaders(String method) {
+
+        JwtToken jwtToken =  jwtTokenRepository.findById(session.getId()).orElseThrow(UnAuthorizedException::new);
+
         HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(jwtToken.getToken());
+
         if (!List.of("GET", "DELETE").contains(method)) { // GET, DELETE 이외 방식은 모두 Body 데이터 있다.
             headers.setContentType(MediaType.APPLICATION_JSON);
         }
