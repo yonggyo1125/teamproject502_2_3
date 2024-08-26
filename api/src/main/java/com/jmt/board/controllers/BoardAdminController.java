e62980db7ebdeda51cb49849a7b1346ebd248821
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 14:33:05 +0900

    2,3조 찜하기 처리

--- api/src/main/java/com/jmt/board/controllers/BoardAdminController.java
+++ api/src/main/java/com/jmt/board/controllers/BoardAdminController.java
@@ -6,7 +6,6 @@ import com.jmt.global.ListData;
 import com.jmt.global.constants.DeleteStatus;
 import com.jmt.global.rests.JSONData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,19 +15,21 @@ public class BoardAdminController {
 
     private final BoardInfoService boardInfoService;
 
-    @GetMapping("/search")
+    @GetMapping // 목록 조회
     public JSONData getList(BoardDataSearch search) {
         ListData<BoardData> data = boardInfoService.getList(search, DeleteStatus.ALL);
 
         return new JSONData(data);
     }
 
-    @PatchMapping
-    public ResponseEntity<Void> listUpdate(@RequestBody RequestAdminList form) {
+    @PatchMapping("/{mode}") // 목록 수정, 삭제
+    public JSONData updatelist(@PathVariable("mode") String mode, @RequestBody RequestAdminList form) {
 
 
-        return ResponseEntity.ok().build();
+        return null;
     }
+
+    
 
     @GetMapping("/info/{seq}")
     public JSONData getInfo(Long seq) {
---
+++ api/src/main/java/com/jmt/board/services/admin/BoardAdminService.java
@@ -0,0 +1,73 @@
+package com.jmt.board.services.admin;
+
+import com.jmt.board.entities.BoardData;
+import com.jmt.board.repositories.BoardDataRepository;
+import com.jmt.global.exceptions.BadRequestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardAdminService {
+    private final BoardDataRepository dataRepository;
+    private final PasswordEncoder encoder;
+
+    public List<BoardData> update(String mode, List<BoardData> items) {
+
+        if (items == null || items.isEmpty()) {
+            String modeStr = mode.equals("delete") ? "삭제" : "수정";
+            throw new BadRequestException(String.format("%s할 게시글을 선택하세요.", modeStr));
+        }
+
+        List<BoardData> updateItems = new ArrayList<>();
+        for (BoardData item : items) {
+            BoardData original = dataRepository.findById(item.getSeq()).orElse(null);
+            if (original == null) continue;
+
+            if (mode.equals("delete")) { // 삭제
+                dataRepository.delete(original);
+            } else { // 수정
+                original.setPoster(item.getPoster());
+                original.setSubject(item.getSubject());
+                original.setContent(item.getContent());
+                original.setCategory(item.getCategory());
+                original.setEditorView(item.getBoard().isUseEditor());
+
+                original.setNum1(item.getNum1());
+                original.setNum2(item.getNum2());
+                original.setNum3(item.getNum3());
+
+                original.setText1(item.getText1());
+                original.setText2(item.getText2());
+                original.setText3(item.getText3());
+
+                original.setLongText1(item.getLongText1());
+                original.setLongText2(item.getLongText2());
+
+                // 비회원 비밀번호 처리
+                String guestPw = item.getGuestPw();
+                if (StringUtils.hasText(guestPw)) {
+                    original.setGuestPw(encoder.encode(guestPw));
+                }
+
+                original.setNotice(item.isNotice());
+                updateItems.add(original);
+            }
+        }
+
+        if (mode.equals("delete")) {
+            dataRepository.flush();
+
+            return items;
+        } else { // 수정
+            dataRepository.saveAllAndFlush(updateItems);
+            return updateItems;
+        }
+
+    }
+}
