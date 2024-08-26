df6bd33614a91bd0373c828834900bfc3df260f8
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 15:01:08 +0900

    2,3조 찜하기 처리

--- admin/src/main/java/com/jmt/board/services/BoardInfoService.java
+++ admin/src/main/java/com/jmt/board/services/BoardInfoService.java
@@ -1,5 +1,10 @@
 package com.jmt.board.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jmt.board.controllers.BoardDataSearch;
+import com.jmt.board.entities.BoardData;
+import com.jmt.global.ListData;
 import com.jmt.global.Utils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,9 +14,18 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class BoardInfoService {
     private final RestTemplate restTemplate;
+    private final ObjectMapper om;
     private final Utils utils;
 
-    public void getList() {
+    public ListData<BoardData> getList(BoardDataSearch search) {
+        String url = utils.url("/board/admin", "api-service");
 
+        try {
+            String jsonParams = om.writeValueAsString(search);
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
