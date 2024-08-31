f7df00ed0136bd5ae82320399c42bdd8f5da077a
yonggyo1125 <yonggyo00@kakao.com>
2024-08-31 09:52:32 +0900

    .

---
+++ real_team3/react_joyfarm-master/src/commons/components/SelectLocation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
+
+
+
+const SelectLocation = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default React.memo(SelectLocation);
