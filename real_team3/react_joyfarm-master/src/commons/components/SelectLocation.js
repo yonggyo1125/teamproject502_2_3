2050fa3841181ca86cb66c32f01978df3a73c73b
yonggyo1125 <yonggyo00@kakao.com>
2024-08-31 10:09:21 +0900

    .

--- real_team3/react_joyfarm-master/src/commons/components/SelectLocation.js
+++ real_team3/react_joyfarm-master/src/commons/components/SelectLocation.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import areas from '../libs/areas';
+const { sido, sigungu } = areas;
 
 const Wrapper = styled.div``;
 
+const SelectLocation = ({ fields }) => {
+  const { t } = useTranslation();
+  const sidoName = fields?.sido ?? 'sido';
+  const sigunguName = fields?.sigungu ?? 'sigungu';
 
+  const [sigunguArea, setSigunguArea] = useState([]);
 
-const SelectLocation = () => {
-  return <Wrapper></Wrapper>;
+  const onChange = useCallback((e) => {
+
+  }, []);
+  return (
+    <Wrapper>
+      <select name={sidoName} onChange={onChange}>
+        <option value="">{t('시도_선택')}</option>
+        {sido.map((s) => (
+          <option key={s} value={s}>
+            {s}
+          </option>
+        ))}
+      </select>
+      <select name={sigunguName} onChange={onChange}>
+        <option value="">{t('시군구_선택')}</option>
+        {sigunguArea &&
+          sigunguArea.length > 0 &&
+          sigunguArea.map((area) => (
+            <option key={area} value={area}>
+              {area}
+            </option>
+          ))}
+      </select>
+    </Wrapper>
+  );
 };
 
 export default React.memo(SelectLocation);
